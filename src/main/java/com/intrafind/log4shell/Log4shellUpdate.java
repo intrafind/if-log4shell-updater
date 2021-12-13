0513de17676966d765f9f06e9a4e535cc67afce8
Adrian Diemer <adrian.diemer@intrafind.de>
2021-12-13 13:40:52 +0100

    initial commit

--- src/main/java/com/intrafind/log4shell/Log4shellUpdate.java
+++ src/main/java/com/intrafind/log4shell/Log4shellUpdate.java
@@ -1,2 +1,153 @@
-package com.intrafind.log4shell;public class Log4shellUpdate {
+package com.intrafind.log4shell;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Log4shellUpdate {
+
+  private static final Pattern VERSION_PATTERN = Pattern.compile("-2\\.(\\d+)\\.\\d+\\.jar");
+  private static Map<String, String> replacements;
+
+  static {
+    try {
+      initReplacementMap();
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+  static final Map<Path, String> TO_REPLACE = new HashMap<>();
+
+  public static void main(String[] args) throws IOException {
+    CommandLine cmd = parseCmd(args);
+    String path = cmd.getOptionValue("path");
+
+    Files.walk(Paths.get(path))
+        .forEach(Log4shellUpdate::addToReplaceIfOldLog4j);
+    if (cmd.hasOption("dry-run")) {
+      TO_REPLACE.forEach((oldFile, replacement) -> System.out.println("Would replace " + oldFile + " with " + replacement));
+    } else {
+      TO_REPLACE.forEach((oldFile, replacement) -> {
+        try {
+          Files.delete(oldFile);
+        } catch (IOException e) {
+          System.err.println("Could not delete " + path + "! Make sure you have write permissions and the file is not in use. Then restart the utility.");
+          throw new IllegalStateException(e);
+        }
+        final Path pathReplacement = oldFile.getParent().resolve(replacement);
+        exportResource(replacement, pathReplacement);
+        System.out.println("Replaced " + oldFile + " with " + replacement);
+      });
+    }
+  }
+
+
+  private static Optional<String> getReplacementIfOld(Path path) {
+    final Matcher matcher = VERSION_PATTERN.matcher(path.getFileName().toString());
+    if (matcher.find()) {
+      if (Integer.parseInt(matcher.group(1)) < 15) {
+        return replacements.entrySet().stream()
+            .filter(entry -> path.getFileName().toString().startsWith(entry.getKey()))
+            .map(Map.Entry::getValue)
+            .findFirst();
+      }
+    }
+      return Optional.empty();
+  }
+
+  private static void initReplacementMap() throws IOException {
+    replacements = new HashMap<>();
+    replacements.put("log4j-1.2-api-", "log4j-1.2-api-2.15.0.jar");
+    replacements.put("log4j-api-", "log4j-api-2.15.0.jar");
+    replacements.put("log4j-core-", "log4j-core-2.15.0.jar");
+    replacements.put("log4j-jcl-", "log4j-jcl-2.15.0.jar");
+    replacements.put("log4j-layout-template-json-", "log4j-layout-template-json-2.15.0.jar");
+    replacements.put("log4j-slf4j-impl-", "log4j-slf4j-impl-2.15.0.jar");
+  }
+
+
+  private static void addToReplaceIfOldLog4j(Path path) {
+    if (path.toFile().isDirectory()) {
+      return;
+    }
+
+    Optional<String> replacement = getReplacementIfOld(path);
+    if (!replacement.isPresent()) {
+      return;
+    }
+
+    final String replacementResource = replacement.get();
+    TO_REPLACE.put(path, replacementResource);
+  }
+
+
+  private static CommandLine parseCmd(String[] args) {
+    Options options = new Options();
+    options.addRequiredOption("p", "path", true, "path to iFinder service");
+    options.addOption("d", "dry-run", false, "only print replacements, do not replace files");
+    options.addOption("h", "help", false, "print this help");
+    CommandLineParser parser = new DefaultParser();
+
+    CommandLine cmd;
+    try {
+      cmd = parser.parse(options, args);
+      if (cmd.hasOption("help")) {
+        new HelpFormatter().printHelp("if-log4shell-updater", options);
+        System.exit(0);
+      }
+    } catch (ParseException e) {
+      System.err.println(e.getMessage());
+      new HelpFormatter().printHelp("if-log4shell-updater", options);
+      System.exit(1);
+      return null;
+    }
+    return cmd;
+  }
+
+
+  private static void exportResource(String resourceName, Path exportPath) {
+    try {
+      OutputStream resStreamOut = null;
+      try (InputStream stream = Log4shellUpdate.class.getResourceAsStream("/" + resourceName)) {
+        if (stream == null) {
+          throw new IllegalStateException("Cannot get resource \"" + resourceName + "\" from Jar file.");
+        }
+
+        int readBytes;
+        byte[] buffer = new byte[4096];
+        final File exportFile = exportPath.toFile();
+        if (exportFile.exists()) {
+          System.out.println("Replacement " + exportPath + " already exists. Nothing to do.");
+        }
+        Files.createFile(exportPath);
+        resStreamOut = new FileOutputStream(exportFile);
+        while ((readBytes = stream.read(buffer)) > 0) {
+          resStreamOut.write(buffer, 0, readBytes);
+        }
+      } finally {
+        if (resStreamOut != null) {
+          resStreamOut.close();
+        }
+      }
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+  }
 }
--- src/test/java/com/intrafind/log4shell/Log4shellUpdateTest.java
+++ src/test/java/com/intrafind/log4shell/Log4shellUpdateTest.java
@@ -1,4 +1,125 @@
-import static org.junit.Assert.*;
+package com.intrafind.log4shell;
+
+import org.hamcrest.CustomMatcher;
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 public class Log4shellUpdateTest {
-  
+
+  private Path tempDir;
+
+  @Before
+  public void before() {
+    try {
+      tempDir = Files.createTempDirectory("test-log4shell");
+      Files.createFile(tempDir.resolve("log4j-1.2-api-2.11.0.jar"));
+      Files.createFile(tempDir.resolve("log4j-api-2.11.0.jar"));
+      Files.createFile(tempDir.resolve("log4j-core-2.11.0.jar"));
+      Files.createFile(tempDir.resolve("log4j-jcl-2.11.0.jar"));
+      Files.createFile(tempDir.resolve("log4j-layout-template-json-2.11.0.jar"));
+      Files.createFile(tempDir.resolve("log4j-slf4j-impl-2.11.0.jar"));
+      Files.createFile(tempDir.resolve("log4j-1.2.17.jar"));
+      final Path service = Files.createDirectory(tempDir.resolve("service"));
+      Files.createFile(service.resolve("log4j-1.2-api-2.11.0.jar"));
+      Files.createFile(service.resolve("log4j-api-2.11.0.jar"));
+      Files.createFile(service.resolve("log4j-core-2.11.0.jar"));
+      Files.createFile(service.resolve("log4j-jcl-2.11.0.jar"));
+      Files.createFile(service.resolve("log4j-layout-template-json-2.11.0.jar"));
+      Files.createFile(service.resolve("log4j-slf4j-impl-2.11.0.jar"));
+      Files.createFile(service.resolve("log4j-1.2.17.jar"));
+      final Path serviceSubOld = Files.createDirectory(service.resolve("subOld"));
+      Files.createFile(serviceSubOld.resolve("log4j-1.2-api-2.11.0.jar"));
+      Files.createFile(serviceSubOld.resolve("log4j-api-2.11.0.jar"));
+      Files.createFile(serviceSubOld.resolve("log4j-core-2.11.0.jar"));
+      Files.createFile(serviceSubOld.resolve("log4j-jcl-2.11.0.jar"));
+      Files.createFile(serviceSubOld.resolve("log4j-layout-template-json-2.11.0.jar"));
+      Files.createFile(serviceSubOld.resolve("log4j-slf4j-impl-2.11.0.jar"));
+      Files.createFile(serviceSubOld.resolve("log4j-1.2.17.jar"));
+      final Path serviceSubNew = Files.createDirectory(service.resolve("subNew"));
+      Files.createFile(serviceSubNew.resolve("log4j-core-2.15.0.jar"));
+      final Path serviceSubNewer = Files.createDirectory(service.resolve("subNewer"));
+      Files.createFile(serviceSubNewer.resolve("log4j-core-2.16.0.jar"));
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testDryRun() throws IOException {
+    Log4shellUpdate.main(new String[]{"-p", tempDir.resolve("service").toString(), "-d"});
+
+    assertThat(Log4shellUpdate.TO_REPLACE.size(), is(equalTo(12)));
+    assertThat(tempDir.resolve("service").toFile().list().length, is(equalTo(10)));
+    assertThat(tempDir.resolve("log4j-core-2.11.0.jar"), exists());
+    assertThat(tempDir.resolve("log4j-core-2.15.0.jar"), not(exists()));
+    assertThat(tempDir.resolve("service/log4j-core-2.11.0.jar"), exists());
+    assertThat(tempDir.resolve("service/log4j-core-2.15.0.jar"), not(exists()));
+    assertThat(tempDir.resolve("service/subOld/log4j-core-2.11.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subOld/log4j-core-2.15.0.jar"), not(exists()));
+    assertThat(tempDir.resolve("service/subNew/log4j-core-2.15.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subNewer/log4j-core-2.16.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subNewer/log4j-core-2.15.0.jar"), not(exists()));
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void test() throws IOException {
+    Log4shellUpdate.main(new String[]{"-p", tempDir.resolve("service").toString()});
+
+    assertThat(Log4shellUpdate.TO_REPLACE.size(), is(equalTo(12)));
+    assertThat(tempDir.resolve("service").toFile().list().length, is(equalTo(10)));
+    assertThat(tempDir.resolve("log4j-core-2.11.0.jar"), exists());
+    assertThat(tempDir.resolve("log4j-core-2.15.0.jar"), not(exists()));
+    assertThat(tempDir.resolve("service/log4j-core-2.11.0.jar"), not(exists()));
+    assertThat(tempDir.resolve("service/log4j-core-2.15.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subOld/log4j-core-2.11.0.jar"), not(exists()));
+    assertThat(tempDir.resolve("service/subOld/log4j-core-2.15.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subNew/log4j-core-2.15.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subNewer/log4j-core-2.16.0.jar"), exists());
+    assertThat(tempDir.resolve("service/subNewer/log4j-core-2.15.0.jar"), not(exists()));
+  }
+
+  @After
+  public void after() throws IOException {
+    Log4shellUpdate.TO_REPLACE.clear();
+    Files.walkFileTree(tempDir, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
+
+  private static <T> Matcher<T> exists() {
+    return new CustomMatcher<T>("exists") {
+      @Override
+      public boolean matches(Object item) {
+        if (item instanceof Path) {
+          return ((Path) item).toFile().exists();
+        } else {
+          return false;
+        }
+      }
+    };
+  }
 }
